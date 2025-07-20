334badd1bfdfa7e43af0fff48ed715a7ba9d765e
Naila Sa <naila2mcs@gmail.com>
2025-07-20 19:50:49 +0500

    Time: 3 ms (99%), Space: 18 MB (68.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()  # Convert the entire string to lowercase for case-insensitive comparison
+        #s = s.lower()  # Convert the entire string to lowercase for case-insensitive comparison
         l, r = 0, len(s) - 1  # Initialize two pointers: left (l) at start, right (r) at end
 
         while l < r:  # Loop until the two pointers meet or cross
@@ -12,13 +12,14 @@ class Solution:
                 l += 1
                 continue
 
-            if s[r] != s[l]:  # If the characters at left and right pointers don't match
+            if s[r].lower() != s[l].lower():  # If the characters at left and right pointers don't match
                 return False  # It's not a palindrome
 
             l += 1  # Move left pointer forward
             r -= 1  # Move right pointer backward
 
         return True   # All characters matched; it's a palindrome
+
 
         # Time O(n)
         # Space O(1)
