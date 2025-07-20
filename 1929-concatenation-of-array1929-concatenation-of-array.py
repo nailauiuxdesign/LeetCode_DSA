6f3c73dee28ac1c370688e6599b36344b330b1d0
Naila Sa <naila2mcs@gmail.com>
2025-07-20 22:40:06 +0500

    Delete 1929-concatenation-of-array1929-concatenation-of-array.py

--- 1929-concatenation-of-array1929-concatenation-of-array.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        for e in nums[0::]:  # Loop through a copy of the list
-            nums.append(e)   # Add each item to the end
-        return nums          # Return the extended list
