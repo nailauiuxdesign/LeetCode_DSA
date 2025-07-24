3a18ada0007b96e08b632de77dc83033819d5c12
Naila Sa <naila2mcs@gmail.com>
2025-07-24 15:36:49 +0500

    Time: 0 ms (100%), Space: 19.1 MB (9.66%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,12 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        l = 0
+        l = 0  # Pointer to track the position to place the next non-zero element
+
+        # Move all non-zero elements to the front
         for r in nums:
             if r != 0:
                 nums[l] = r
                 l += 1
 
+        # Fill the rest of the list with zeros
         for i in range(l, len(nums)):
             nums[i] = 0
 
