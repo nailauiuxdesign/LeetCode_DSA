424a347ac9097addd1e1190a08a530454919cc75
Naila Sa <naila2mcs@gmail.com>
2025-07-24 15:23:20 +0500

    Time: 7 ms (43.26%) | Memory: 19 MB (30.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,8 +5,8 @@ class Solution:
         """
         l = 0
 
-        for r, value in enumerate(nums):
-            if value != 0:
+        for r in range(len(nums)):
+            if nums[r]:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
 
