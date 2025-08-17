e3c933f0677749611ee4b453770c0ce39f37242f
Naila Sa <naila2mcs@gmail.com>
2025-08-17 16:37:50 +0500

    Time: 31 ms (93.59%), Space: 17.63 MB (63.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # previous, current = 0, 1
+        previous, current = 0, 1
       
-        # for _ in range(n):
-        #     previous, current = current, previous + current
+        for _ in range(n):
+            previous, current = current, previous + current
       
-        # return previous
-
-        # Base case: 0 or 1
-        if n == 0 or n == 1:
-            return n
-        # Recursive case: sum of two previous numbers
-        return self.fib(n - 1) + self.fib(n - 2)
-
+        return previous
