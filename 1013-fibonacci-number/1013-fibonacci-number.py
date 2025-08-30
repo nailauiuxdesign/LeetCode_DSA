876d6d441e4481e2e1839dfecceb637de1a06d6f
Naila Sa <naila2mcs@gmail.com>
2025-08-30 12:49:58 +0500

    Delete 1013-fibonacci-number directory

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        previous, current = 0, 1
-      
-        for _ in range(n):
-            previous, current = current, previous + current
-      
-        return previous
