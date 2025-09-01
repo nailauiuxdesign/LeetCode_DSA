a4bda6fe52672cbcb4e938c0ee0535e77ccdcbe7
Naila Sa <naila2mcs@gmail.com>
2025-09-01 12:08:09 +0500

    Time: 119 ms (59.98%) | Memory: 18.2 MB (23.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,31 +1,14 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        longest = 0
-        l = 0
-        counts = [0] * 26
- 
-        for r in range(len(s)):
-            counts[ord(s[r]) - 65] += 1
- 
-            while (r - l + 1) - max(counts) > k:
-                counts[ord(s[l]) - 65] -= 1
-                l += 1
- 
-            longest = max(longest, (r - l + 1))
- 
-        return longest
-        
-        
-        
-        # left = 0
-        # max_freq = 0
-        # max_window = 0
-        # freq = defaultdict(int)
-        # for right in range(len(s)):
-        #     freq[s[right]] +=1
-        #     max_freq = max(max_freq, freq[s[right]])
-        #     if (right-left+1) - max_freq > k:
-        #         freq[s[left]]-=1
-        #         left+=1
-        #     max_window= max(max_window, right-left+1)
-        # return max_window
+    def characterReplacement(self, s: str, k: int) -> int:           
+        left = 0
+        max_freq = 0
+        max_window = 0
+        freq = defaultdict(int)
+        for right in range(len(s)):
+            freq[s[right]] +=1
+            max_freq = max(max_freq, freq[s[right]])
+            if (right-left+1) - max_freq > k:
+                freq[s[left]]-=1
+                left+=1
+            max_window= max(max_window, right-left+1)
+        return max_window
