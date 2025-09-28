9958618b941f8fb0130c3a648dbb601997c04784
Naila Sa <naila2mcs@gmail.com>
2025-09-28 18:10:29 +0500

    Time: 0 ms (100%), Space: 17.7 MB (75.96%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -1,4 +1,13 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        count = collections.Counter(text)
-        return min(count['b'], count['a'], count['l'] // 2, count['o'] // 2, count['n'])
+        count = defaultdict(int)
+        balloon = "balloon"
+        for c in text:
+            if c in balloon:
+                count[c] += 1
+        if any(c not in count for c in balloon):
+            return 0
+        else:
+            return min(count["b"], count["a"], count["l"] // 2, count["o"] // 2, count["n"])
+
+ 
