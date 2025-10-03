8295882124d06b430d529ba1d0e5a445a1230d47
Naila Sa <naila2mcs@gmail.com>
2025-10-03 13:14:52 +0500

    Time: 34 ms (80.83%) | Memory: 17.6 MB (74.26%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,18 +1,17 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        m, n = len(word1), len(word2)
         new_word = []
-        if len(word1) < len(word2):
-            for i in range(len(word1)):
-                new_word.append(word1[i])
-                new_word.append(word2[i])
-            
-            new_word.append(word2[len(word1):])
 
+        # Merge up to the shorter length
+        for i in range(min(m, n)):
+            new_word.append(word1[i])
+            new_word.append(word2[i])
+
+        # Add leftover characters (if any)
+        if m > n:
+            new_word.extend(word1[n:])
         else:
-            for i in range(len(word2)):
-                new_word.append(word1[i])
-                new_word.append(word2[i])
-            
-            new_word.append(word1[len(word2):])
+            new_word.extend(word2[m:])
 
         return ''.join(new_word)
