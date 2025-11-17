823aa447dfa33c7db4b0234772dceb4f039c9714
Naila Sa <naila2mcs@gmail.com>
2025-11-17 19:37:30 +0500

    Delete 0048-rotate-image directory

--- 0048-rotate-image/0048-rotate-image.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        matrix.reverse()
-        for i, j in itertools.combinations(range(len(matrix)), 2):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
