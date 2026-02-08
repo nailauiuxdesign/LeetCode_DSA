f111c04ee513c80def2399b78696f1cb076081e2
Naila Sa <naila2mcs@gmail.com>
2026-02-08 12:51:32 +0500

    Time: 0 ms (100%), Space: 19.3 MB (55.89%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def mincostTickets(self, days: list[int], costs: list[int]) -> int:
+        cost = 0
+        week = deque()
+        month = deque()
+
+        for d in days:
+            while week and week[0][0] + 7 <= d:
+                week.popleft()
+            while month and month[0][0] + 30 <= d:
+                month.popleft()
+
+            week.append((d, cost + costs[1]))
+            month.append((d, cost + costs[2]))
+
+            cost = min(
+                cost + costs[0],
+                week[0][1],
+                month[0][1]
+            )
+
+        return cost
