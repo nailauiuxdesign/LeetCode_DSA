ae4a073a9dda8db02b4f6a11d6fd4e7296f37c07
Naila Sa <naila2mcs@gmail.com>
2026-02-12 19:36:09 +0500

    Time: 8 ms (22.69%) | Memory: 21.3 MB (18.21%) - LeetSync

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,6 +1,5 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        
         graph = [[] for _ in range(numCourses)]
         state = [0] * numCourses
 
