1d109383269884bad05eeefb999d9800afe44548
Naila Sa <naila2mcs@gmail.com>
2026-02-12 19:37:43 +0500

    Time: 346 ms (60.07%) | Memory: 97.1 MB (54.43%) - LeetSync

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from typing import List
 
-
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         if source == destination:
