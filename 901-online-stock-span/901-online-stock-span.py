be969e97642f5a7f6d4f087d5384fab07a0f4cf1
Naila Sa <naila2mcs@gmail.com>
2026-03-23 12:20:10 +0500

    Time: 46 ms (90.30%) | Memory: 23.9 MB (78.10%) - LeetSync

--- 901-online-stock-span/901-online-stock-span.py
+++ 901-online-stock-span/901-online-stock-span.py
@@ -6,6 +6,5 @@ class StockSpanner:
         span = 1
         while self.stack and self.stack[-1][0] <= price:
             span += self.stack.pop()[1]
-
         self.stack.append((price, span))
         return span
