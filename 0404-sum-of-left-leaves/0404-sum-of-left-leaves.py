a12bf491ef6102e9a64ccffccc476d16aa32d204
Naila Sa <naila2mcs@gmail.com>
2026-04-08 17:11:18 +0500

    Delete 0404-sum-of-left-leaves directory

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        if root.left and root. left.left is None and root. left.right is None:
-            return root.left.val + self.sumOfLeftLeaves(root.right)
-        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
