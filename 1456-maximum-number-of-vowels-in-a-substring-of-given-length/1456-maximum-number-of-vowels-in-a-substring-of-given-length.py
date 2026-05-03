ecf7c005b3322a1ece67bf3959b1a0941c309601
Naila Sa <naila2mcs@gmail.com>
2026-05-03 12:59:46 +0500

    Time: 49 ms (72.95%) | Memory: 19.8 MB (66.94%) - LeetSync

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -14,6 +14,7 @@ class Solution:
                 count += 1
             if s[i - k] in vowels:
                 count -= 1
+                
             max_count = max(max_count, count)
 
         return max_count
