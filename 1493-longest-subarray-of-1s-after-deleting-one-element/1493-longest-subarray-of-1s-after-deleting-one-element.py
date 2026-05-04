652d2742d9fcc2cf2461b4fddb8b601cf69c5961
Naila Sa <naila2mcs@gmail.com>
2026-05-04 12:48:50 +0500

    Time: 31 ms (88.82%) | Memory: 24.6 MB (6.83%) - LeetSync

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -3,9 +3,11 @@ class Solution:
         max_len = 0
         start = 0
         zero = -1
+        
         for i in range(len(nums)):
             if nums[i] ==0:
                 start = zero+1
                 zero=i
             max_len=max(max_len, i-start)
+            
         return max_len
