2671722184ae53897509d32ace5d1307bd647378
Naila Sa <naila2mcs@gmail.com>
2026-05-07 13:09:37 +0500

    Time: 11 ms (100%) | Memory: 20.3 MB (70.65%) - LeetSync

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -2,16 +2,12 @@ class Solution:
     def minDominoRotations(self, tops: list[int], bottoms: list[int]) -> int:
         for num in range(1, 7):
             possible = True
-
             for i in range(len(tops)):
                 if tops[i] != num and bottoms[i] != num:
                     possible = False
                     break
-
             if possible:
                 top_count = tops.count(num)
                 bottom_count = bottoms.count(num)
-
                 return len(tops) - max(top_count, bottom_count)
-
         return -1
