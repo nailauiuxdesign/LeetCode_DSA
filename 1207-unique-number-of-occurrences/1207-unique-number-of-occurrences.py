5cc8617ae440a8282b3bb757f435630fce7697d7
Naila Sa <naila2mcs@gmail.com>
2026-05-26 10:57:17 +0500

    Time: 0 ms (100%), Space: 19.4 MB (39.5%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def uniqueOccurrences(self, arr):
+        count = defaultdict(int)
+
+        for num in arr:
+            count[num] += 1
+        occurrences = count.values()
+
+        return len(occurrences) == len(set(occurrences))
