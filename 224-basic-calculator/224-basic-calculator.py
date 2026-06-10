38f0f2c8942e7d964e180b8678ac322fc40ad35b
Naila Sa <naila2mcs@gmail.com>
2026-06-10 12:31:22 +0500

    Time: 35 ms (86.22%) | Memory: 20.4 MB (33.97%) - LeetSync

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -8,23 +8,19 @@ class Solution:
         for char in s:
             if char.isdigit():
                 num = num * 10 + int(char)
-
             elif char == "+":
                 result += sign * num
                 num = 0
                 sign = 1
-
             elif char == "-":
                 result += sign * num
                 num = 0
                 sign = -1
-
             elif char == "(":
                 stack.append(result)
                 stack.append(sign)
                 result = 0
                 sign = 1
-
             elif char == ")":
                 result += sign * num
                 num = 0
