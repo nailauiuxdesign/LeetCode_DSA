6aa1efef14112f4a6c0af4cbc3fc2e468504149b
Naila Sa <naila2mcs@gmail.com>
2026-06-11 10:37:08 +0500

    Time: 55 ms (39.25%) | Memory: 19.9 MB (63.76%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        rev = s[::-1]
+
+        for i in range(len(s) + 1):
+            if s.startswith(rev[i:]):
+                return rev[:i] + s
+
+        return s
