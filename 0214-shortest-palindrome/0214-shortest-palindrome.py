406bce5efce230ef8bd51333d09229dd7743d114
Naila Sa <naila2mcs@gmail.com>
2026-06-11 10:38:56 +0500

    Delete 0214-shortest-palindrome directory

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def shortestPalindrome(self, s: str) -> str:
-        rev = s[::-1]
-
-        for i in range(len(s) + 1):
-            if s.startswith(rev[i:]):
-                return rev[:i] + s
-
-        return s
