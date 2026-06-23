26943ad1308d3bae9bad91c119de095957eb95d3
Naila Sa <naila2mcs@gmail.com>
2026-06-23 20:17:58 +0500

    Time: 3 ms (48.80%) | Memory: 19.3 MB (63.82%) - LeetSync

--- 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.py
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.py
@@ -1,7 +1,9 @@
 class Solution:
     def isAdjacentDiffAtMostTwo(self, s: str) -> bool:
         N = len(s) -1
+        
         for i in range(N):
             if abs(int(s[i]) - int(s[i +1])) >2:
                 return False
+                
         return True
