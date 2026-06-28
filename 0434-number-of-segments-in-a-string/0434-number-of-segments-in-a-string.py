5cebf728f5022104094c43c6b5ba39fe0027f5fb
Naila Sa <naila2mcs@gmail.com>
2026-06-28 11:56:07 +0500

    Delete 0434-number-of-segments-in-a-string directory

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countSegments(self, s):
-        count = 0
-
-        for i in range(len(s)):
-            if s[i] != " " and (i == 0 or s[i - 1] == " "):
-                count += 1
-
-        return count
