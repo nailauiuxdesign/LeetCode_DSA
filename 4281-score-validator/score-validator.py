33c5d08f859a3f0a11307b4b58a15fbbce8efeaa
Naila Sa <naila2mcs@gmail.com>
2026-07-12 12:30:32 +0500

    Time: 3 ms (52.05%) | Memory: 19.4 MB (48.51%) - LeetSync

---
+++ 4281-score-validator/score-validator.py
@@ -0,0 +1,19 @@
+class Solution:
+    def scoreValidator(self, events):
+        score = 0
+        wickets = 0
+
+        for event in events:
+            if event == "W":
+                wickets += 1
+
+            elif event == "WD" or event == "NB":
+                score += 1
+
+            else:
+                score += int(event)
+
+            if wickets == 10:
+                break
+
+        return [score, wickets]
