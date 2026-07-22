f226c8e1a28933077de7fa87bdffc1d287654cf2
Naila Sa <naila2mcs@gmail.com>
2026-07-22 21:08:35 +0500

    Time: 0 ms (100%), Space: 19.2 MB (88.02%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countTime(self, time: str) -> int:
+        ways = 1
+
+        if time[3] == '?':
+            ways *= 6
+        if time[4] == '?':
+            ways *= 10
+
+        if time[0] == '?' and time[1] == '?':
+            ways *= 24
+        elif time[0] == '?':
+            if time[1] <= '3':
+                ways *= 3
+            else:
+                ways *= 2
+        elif time[1] == '?':
+            if time[0] == '2':
+                ways *= 4
+            else:
+                ways *= 10
+
+        return ways
