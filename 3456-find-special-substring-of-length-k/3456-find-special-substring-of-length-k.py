0cf32b6f2cf2ddee0de5e07995d269d99779fc01
Naila Sa <naila2mcs@gmail.com>
2026-07-30 21:21:58 +0500

    Time: 0 ms (100.00%) Memory: 19.2 MB (89.16%) - LeetSync

--- 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.py
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.py
@@ -12,8 +12,10 @@ class Solution:
 
             if not same:
                 continue
+                
             if start > 0 and s[start - 1] == ch:
                 continue
+                
             if start + k < n and s[start + k] == ch:
                 continue
 
