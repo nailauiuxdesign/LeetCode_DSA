3429ff357ac86cf38c808f65e8299d12edba8dd3
Naila Sa <naila2mcs@gmail.com>
2026-07-30 21:19:18 +0500

    Time: 0 ms (100%), Space: 19.2 MB (89.16%) - LeetHub

---
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasSpecialSubstring(self, s: str, k: int) -> bool:
+        n = len(s)
+
+        for start in range(n - k + 1):
+            ch = s[start]
+            same = True
+            for i in range(start, start + k):
+                if s[i] != ch:
+                    same = False
+                    break
+
+            if not same:
+                continue
+            if start > 0 and s[start - 1] == ch:
+                continue
+            if start + k < n and s[start + k] == ch:
+                continue
+
+            return True
+
+        return False
